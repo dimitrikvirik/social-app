166e7c998a0e8a40aa7349a5ff17ceb55f357440
dimit <dimitrikvirik@gmail.com>
2023-03-05 20:57:20 +0400

    updated test

--- feed-api/src/test/java/git/dimitrikvirik/feedapi/controller/FeedPostControllerTest.java
+++ feed-api/src/test/java/git/dimitrikvirik/feedapi/controller/FeedPostControllerTest.java
@@ -2,6 +2,14 @@ package git.dimitrikvirik.feedapi.controller;
 
 import git.dimitrikvirik.feedapi.config.ElasticsearchTestContainer;
 import git.dimitrikvirik.generated.feedapi.model.PostRequest;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,7 +21,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
 
+import java.io.IOException;
 import java.util.Collections;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -28,7 +38,31 @@ class FeedPostControllerTest {
 
 	@BeforeAll
 	static void setUp() {
-		ElasticsearchTestContainer.getContainer().start();
+		try (ElasticsearchContainer container = new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:8.5.3")
+				.withPassword("elastic")
+		) {
+			// Start the container. This step might take some time...
+			container.start();
+
+			// Do whatever you want with the rest client ...
+			final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+			credentialsProvider.setCredentials(
+					AuthScope.ANY,
+					new UsernamePasswordCredentials("elastic", "elastic")
+			);
+
+			var client =
+					RestClient
+							.builder(HttpHost.create(container.getHttpHostAddress()))
+							.setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
+							.build();
+
+			Response response = client.performRequest(new Request("GET", "/_cluster/health"));
+			System.out.println(response);
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Test
